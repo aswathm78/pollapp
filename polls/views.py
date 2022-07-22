f9dd33786ded5145fcc89c62a80ca6f7ff7caf66
Aswath Muralidharan <aswathm78@gmail.com>
2022-07-22 20:54:41 +0530

    Add new methods to views

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("hi")
+
+def details(request, question_id):
+    return HttpResponse("You are looking at the questions {question_id}")
+
+def results(request, question_id):
+    return HttpResponse("The results of the question are {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse("The choices of the questions are {question_id}")
