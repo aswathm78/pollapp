e5e018ba0d72876b1b338aef1387cfff20983555
Aswath Muralidharan <aswathm78@gmail.com>
2022-07-22 21:42:32 +0530

    Modify views for better responses

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,27 @@
-from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from .models import Question
 
+from django.shortcuts import render
+
+
 # Create your views here.
 def index(request):
-    result = [x for x in Question.objects.all()]
-    return HttpResponse(result)
+    result = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'result': result,
+    }
+    return render(request,'polls/index.html',context)
 
 def details(request, question_id):
-    return HttpResponse("You are looking at the questions {}".format(question_id))
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("That id does not exist")
+    return render(request, 'polls/details.html', context={'question': question})
+
 
 def results(request, question_id):
     return HttpResponse("The results of the question are {}".format(question_id))
