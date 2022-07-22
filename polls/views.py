ac374f70e9a9792853f7b9e9b02f9e61dfadc361
Aswath Muralidharan <aswathm78@gmail.com>
2022-07-22 20:59:53 +0530

    Change index to display all questions

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hi")
+    result = [x for x in Question.objects.all()]
+    return HttpResponse(result)
 
 def details(request, question_id):
-    return HttpResponse("You are looking at the questions {question_id}")
+    return HttpResponse("You are looking at the questions {}".format(question_id))
 
 def results(request, question_id):
-    return HttpResponse("The results of the question are {question_id}")
+    return HttpResponse("The results of the question are {}".format(question_id))
 
 def vote(request, question_id):
-    return HttpResponse("The choices of the questions are {question_id}")
+    return HttpResponse("The choices of the questions are {}".format(question_id))
